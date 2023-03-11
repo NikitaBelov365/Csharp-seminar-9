78b9eff5dc7558fdcfbb32fabcef7723eee9b0cd
Nikitabelov365 <nikitabelov365@gmail.com>
2023-03-11 18:08:05 +0300

    task 4 solved

---
+++ task 4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 4: Напишите программу, 
+// которая на вход принимает два 
+// числа A и B, и возводит число
+// А в целую степень B с помощью рекурсии.
+
+int RecPower (int a, int b)
+{
+    if(b == 0) return 1;
+    else return a * RecPower(a, b-1);
+}
+
+System.Console.WriteLine(RecPower(2, 10));
