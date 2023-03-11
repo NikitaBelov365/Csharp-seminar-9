6946a634e35f3e920f32efcb577a9d6547e04e5e
Nikitabelov365 <nikitabelov365@gmail.com>
2023-03-11 17:23:49 +0300

    task 1 solved

---
+++ task 1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 1: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке 
+// от 1 до N при помощи рекурсии.
+
+void RecNumbers(int number)
+{
+    if (number <= 0)
+    {
+        return;
+    }
+    RecNumbers(number -1);
+    System.Console.Write($"{number} ");
+}
+
+RecNumbers(10);
