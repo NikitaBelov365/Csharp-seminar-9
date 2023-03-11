e432863ab7933308db8df66f1537580646425b0b
Nikitabelov365 <nikitabelov365@gmail.com>
2023-03-11 17:55:00 +0300

    task 3 solved

---
+++ task 3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 3: Напишите программу, 
+// которая будет принимать на вход число 
+// и возвращать сумму его цифр. Использовать рекурсию.
+
+int RecSumDigits(int num)
+{
+    if(num == 0) return 0;
+    return num = num%10 + RecSumDigits(num/10);
+}
+
+System.Console.WriteLine(RecSumDigits(555));
