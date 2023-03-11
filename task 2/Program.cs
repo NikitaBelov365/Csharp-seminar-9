163aba4be5c78f00278ff01b9602dfd96080182b
Nikitabelov365 <nikitabelov365@gmail.com>
2023-03-11 17:54:51 +0300

    task 2 solved

---
+++ task 2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 2: Задайте значения M и N. 
+// Напишите программу, которая рекурсивно 
+// выведет все натуральные числа в промежутке от M до N.
+
+void RecNumbers(int m, int n)
+{
+    if (m > n-2) return;
+    System.Console.Write($"{m+1} ");
+    RecNumbers(m + 1, n);
+}
+
+RecNumbers(5, 10);
