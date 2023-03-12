a58b50c6e9c8841f02a1be3b978fb6101c3e4006
Nikitabelov365 <nikitabelov365@gmail.com>
2023-03-12 10:46:55 +0300

    task 5 solved

---
+++ task 5/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 5: Определите, является ли число степенью двойки:
+
+bool RecBin(int value)
+{
+    if (value == 1) return true;
+    return value % 2 == 0 && RecBin(value / 2);
+}
+
+if(RecBin(1023)) System.Console.WriteLine("Yes");
+else System.Console.WriteLine("No");
