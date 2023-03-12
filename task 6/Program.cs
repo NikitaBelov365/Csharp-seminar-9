0c99680cd7434998c925d2777273ea88fdb252c7
Nikitabelov365 <nikitabelov365@gmail.com>
2023-03-12 12:31:51 +0300

    task 6 solved

---
+++ task 6/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 6: Проверка на простое число:
+
+bool RecPrime(int value, int i)
+{
+    if(i == 1) return true;
+    System.Console.WriteLine(i);
+    return value%i != 0 && RecPrime(value, i-1);
+}
+
+int num = 101, i = num - 1;
+if(RecPrime(num, i)) System.Console.WriteLine("Yes");
+else System.Console.WriteLine("No");
